a1575053916ca700d45d39c38aef465e7ffc17f5
goncharukop <goncharukop@gmail.com>
2020-10-19 22:27:44 +0300

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.2864373d6bc61c7e48f9e43b143db318.js"
+  "./precache-manifest.024b646a4435ca3dc57ea8f665d4b5ef.js"
 );
 
 self.addEventListener('message', (event) => {
